37fb9bfb8f5a20f0ed7c13aaa65cf1303588a3f2
heayr <djtlm@yandex.ru>
2023-08-16 21:14:06 +0300

    mark_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "react-credit-cards-2";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import {
+  formatCreditCardNumber,
+  formatCVC,
+  formatExpirationDate,
+  formatFormData
+} from "./utils";
+
+import 'react-credit-cards-2/dist/es/styles-compiled.css';
+
+export default class App extends React.Component {
+  state = {
+    number: "",
+    name: "",
+    expiry: "",
+    cvc: "",
+    issuer: "",
+    focused: "",
+    formData: null
+  };
+
+  handleCallback = ({ issuer }, isValid) => {
+    if (isValid) {
+      this.setState({ issuer });
+    }
+  };
+
+  handleInputFocus = ({ target }) => {
+    this.setState({
+      focused: target.name
+    });
+  };
+
+  handleInputChange = ({ target }) => {
+    if (target.name === "number") {
+      target.value = formatCreditCardNumber(target.value);
+    } else if (target.name === "expiry") {
+      target.value = formatExpirationDate(target.value);
+    } else if (target.name === "cvc") {
+      target.value = formatCVC(target.value);
+    }
+
+    this.setState({ [target.name]: target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { issuer } = this.state;
+    const formData = [...e.target.elements]
+      .filter(d => d.name)
+      .reduce((acc, d) => {
+        acc[d.name] = d.value;
+        return acc;
+      }, {});
+
+    this.setState({ formData });
+    this.form.reset();
+  };
+
+  render() {
+    const { name, number, expiry, cvc, focused, issuer, formData } = this.state;
+
+    return (
+      <div key="Payment">
+        <div className="App-payment">
+          <h1>I used a React Library for this test task, because I thought, that speed and accuracy is what is needed for the test task</h1>
+          <Card
+            number={number}
+            name={name}
+            expiry={expiry}
+            cvc={cvc}
+            focused={focused}
+            callback={this.handleCallback}
+          />
+          <form ref={c => (this.form = c)} onSubmit={this.handleSubmit}>
+            <div className="form-group">
+              <input
+                type="tel"
+                name="number"
+                className="form-control"
+                placeholder="Card Number"
+                pattern="[\d| ]{16,22}"
+                required
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                placeholder="Name"
+                required
+                onChange={this.handleInputChange}
+                onFocus={this.handleInputFocus}
+              />
+            </div>
+            <div className="row">
+              <div className="col-6">
+                <input
+                  type="tel"
+                  name="expiry"
+                  className="form-control"
+                  placeholder="Valid Thru"
+                  pattern="\d\d/\d\d"
+                  required
+                  onChange={this.handleInputChange}
+                  onFocus={this.handleInputFocus}
+                />
+              </div>
+              <div className="col-6">
+                <input
+                  type="tel"
+                  name="cvc"
+                  className="form-control"
+                  placeholder="CVC"
+                  pattern="\d{3,4}"
+                  required
+                  onChange={this.handleInputChange}
+                  onFocus={this.handleInputFocus}
+                />
+              </div>
+            </div>
+            <input type="hidden" name="issuer" value={issuer} />
+            <div className="form-actions">
+              <button className="btn btn-primary btn-block">PAY</button>
+            </div>
+          </form>
+          {formData && (
+            <div className="App-highlight">
+              {formatFormData(formData).map((d, i) => (
+                <div key={i}>{d}</div>
+              ))}
+            </div>
+          )}
+         
+        </div>
+       
+      </div>
+    );
+  }
 }
-
-export default App;
